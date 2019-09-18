861d243879a9e4355be2fa97f607cf791f022c28
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-09-18 13:32:10 +0200

    Adds templates to DOM, but without correct student info

--- script.js
+++ script.js
@@ -3,40 +3,61 @@
 document.addEventListener("DOMContentLoaded", getJson);
 
 let originalStudentArray;
+let familyBlood;
 let studentArray = [];
+let sortBy = "lastName";
+let house = "All";
+
+const destStudentList = document.querySelector(".student_list");
+const studentTemplate = document.querySelector(".student_template");
 
 async function getJson() {
-    /* Loads the JSON */
+    // Getting student array
     let pagesUrl = "http://petlatkea.dk/2019/hogwartsdata/students.json";
     let jsonData = await fetch(pagesUrl);
     originalStudentArray = await jsonData.json();
+
+    // Getting list of families and blood status
+    let pagesUrlFam = "http://petlatkea.dk/2019/hogwartsdata/families.json";
+    let jsonDataFam = await fetch(pagesUrlFam);
+    familyBlood = await jsonDataFam.json();
+
     start();
 }
 
 function start() {
     createStudentArray(originalStudentArray);
-    sortFunction("lastName");
-    console.log(studentArray);
+    addMeToList();
+    createFinishedList();
+    showStudentList();
+    addEventListeners();
+    // showStudentList(studentArray);
+}
 
+
+// ----- EVENTLISTENERS -----
+
+function addEventListeners() {
     document.querySelector(".sort").addEventListener("change", function () {
-        let sortBy = this.value;
+        sortBy = this.value;
         console.log(this.value);
-        console.log(sortFunction(sortBy));
+        createFinishedList();
     });
 
     document.querySelector(".filter").addEventListener("change", function () {
-        let house = this.value;
+        house = this.value;
         console.log(this.value);
-        console.log(filterFunction('house', house));
+        createFinishedList();
     });
 }
-
 
 // ----- CREATING THE LIST -----
 
 function createStudentArray(originalArray) {
     originalArray.forEach(student => {
         let thisStudent = createStudentObject(student);
+        thisStudent = addBloodStatus(thisStudent);
+        thisStudent = randomizeBloodStatus(thisStudent);
         studentArray.push(thisStudent);
     });
 }
@@ -49,10 +70,13 @@ function createStudentObject(student) {
 
     return {
         firstName: names.shift(),
+        bloodStatus: null,
         lastName: names.pop() || "Unknown",
         middleName: names.join(" "),
         gender: student.gender,
-        house: capitalize(student.house.trim())
+        house: capitalize(student.house.trim()),
+        isPrefect: false,
+        isInqSquadMember: false
     };
 }
 
@@ -62,22 +86,97 @@ function capitalize(str) {
     return firstLetter.toUpperCase() + lastLetters.toLowerCase();
 }
 
+function addBloodStatus(thisStudent) {
+
+    if (familyBlood.half.includes(thisStudent.lastName)) {
+        thisStudent.bloodStatus = "Halfblood";
+        // console.log("We're half");
+    }
+
+    else if (familyBlood.pure.includes(thisStudent.lastName)) {
+        thisStudent.bloodStatus = "Pureblood";
+        // console.log("We're pure");
+    }
+
+    else {
+        thisStudent.bloodStatus = "Muggleblood";
+    }
+
+    return thisStudent;
+
+}
+
 
 // ----- FILTER AND SORT FUNCTIONS -----
 
-function filterFunction(field, value) {
+function filterFunction(list, field, value) {
     if (value === "All") {
-        return studentArray;
+        return list;
     }
 
-    return studentArray.filter(function (student) {
+    return list.filter(function (student) {
         return student[field] === value;
     });
 }
 
-function sortFunction(sortBy) {
-    studentArray.sort((a,b) => {
+function sortFunction(list, sortBy) {
+    list.sort((a,b) => {
         return a[sortBy].localeCompare(b[sortBy]);
     });
-    return studentArray;
+    return list;
+}
+
+
+// ----- CREATING FINISHED LIST -----
+
+function createFinishedList() {
+    const currentStudentList = filterFunction(sortFunction(studentArray, sortBy), 'house', house);
+    console.log(currentStudentList);
+    console.log(`Showing ${currentStudentList.length} students`);
+}
+
+
+// ----- SHOW IN DOM -----
+
+function showStudentList() {
+    studentArray.forEach(student => {
+        destStudentList.appendChild(studentTemplate.content.cloneNode(true));
+        document.querySelector(".list_first_name").innerHTML = student.firstName;
+    })
+}
+
+
+// ----- HACKING -----
+
+function addMeToList() {
+    let thisStudent = createObjectOfMe();
+    studentArray.push(thisStudent);
+}
+
+function createObjectOfMe() {
+    return {
+        firstName: "Camilla",
+        lastName: "Olsen",
+        middleName: "Gejl",
+        gender: "girl",
+        house: "Hufflepuff",
+        bloodStatus: "Pureblood",
+        isPrefect: false,
+        isInqSquadMember: false
+    };
+}
+
+function randomizeBloodStatus(thisStudent) {
+    if (thisStudent.bloodStatus === "Halfblood" || thisStudent.bloodStatus === "Muggleblood") {
+        thisStudent.bloodStatus = "Pureblood";
+    }
+
+    else {
+        let bloodArray = ["Pureblood", "Halfblood", "Muggleblood"];
+        let randomBlood = Math.floor(Math.random() * Math.floor(3));
+
+        thisStudent.bloodStatus = bloodArray[randomBlood];
+    }
+
+    return thisStudent;
 }
